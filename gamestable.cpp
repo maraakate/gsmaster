3aae1e6dbe9cdccd598926f896c4de79b40a9c08
maraakate <emoaddict15@gmail.com>
2018-01-23 10:49:05 -0500

    heretic2

--- gamestable.cpp
+++ gamestable.cpp
@@ -12,6 +12,7 @@ game_table_t gameTable[] =
 	{"blood2", "jUOF0p", 0},
 	{"daikatana", "fl8aY7", 27991},
 	{"gspylite", "mgNUaC", 0},
+	{"heretic2", "2iuCAS", 0},
 	{"hexenworld", "6SeXQB", 0},
 	{"kingpin", "QFWxY2", 0},
 	{"nolf", "Jn3Ab4", 0},
